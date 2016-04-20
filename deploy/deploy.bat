0d85f806943e010d156fbdcb025c85e59bcf24d8
mattjcowan <mjc@mjczone.com>
2016-04-20 18:57:49 -0500

    simplified nuget deploy script

--- deploy/deploy.bat
+++ deploy/deploy.bat
@@ -14,26 +14,34 @@ SET packageVersion=%2
 GOTO Deploy
 
 :Prompt_version
-SET /P packageVersion=Please enter the version of the package to deploy: 
+SET /P packageVersion=Please enter the version (eg: 1.0.0): 
 IF "%packageVersion%"=="" GOTO Error
 GOTO Deploy
 
 :Error
 echo.
-ECHO You did not enter an api key! Bye bye!!
+ECHO You are missing an api key and/or version number! Bye bye!!
 GOTO End
 
 :Deploy
-"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" ..\src\EasyPlugins\EasyPlugins.csproj /p:Configuration=Release 
-if not exist "..\src\EasyPlugins\EasyPlugins.%packageVersion%.nupkg" (
+call AssemblyVersionInfo.bat %packageVersion%
+"C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" ..\src\EasyPlugins\EasyPlugins.csproj /p:Configuration=Release /nologo /verbosity:m
+robocopy .\ ..\src\EasyPlugins EasyPlugins.nuspec /NJH /NJS
+nuget pack ..\src\EasyPlugins\EasyPlugins.csproj -Properties "Configuration=Release;Platform=AnyCPU" -IncludeReferencedProjects -symbols -NoPackageAnalysis
+del ..\src\EasyPlugins\EasyPlugins.nuspec
+set /P pushPrompt=Would you like to push the package to nuget.org (y/N)? 
+if "%pushPrompt%"=="y" GOTO Push
+if "%pushPrompt%"=="Y" GOTO Push
+if "%pushPrompt%"=="yes" GOTO Push
+GOTO End
+
+:Push
+if not exist "EasyPlugins.%packageVersion%.nupkg" (
     GOTO Package_does_not_exist
 )
-robocopy ..\src\EasyPlugins\ .\ EasyPlugins.%packageVersion%.nupkg /MOV 
-echo.
-echo DEPLOYING!!! You are deploying package EasyPlugins.%packageVersion%.nupkg with API key %apiKey% !!
-echo.
+echo Pushing package EasyPlugins.%packageVersion%.nupkg
+nuget push EasyPlugins.%packageVersion%.nupkg %apiKey%
 GOTO End
-REM nuget push EasyPlugins.%packageVersion%.nupkg %apiKey%
 
 :Package_does_not_exist
 echo.
